1337ef1d29231063dac40c26bbc2ccc2dc828f3d
Eric Rafael <rafaelpsec@gmail.com>
2023-05-15 14:11:41 -0300

    useState in App.jsx not working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,16 @@
-
+import { useState } from "react"
 import Jogo from "./Jogo"
 import Letras from "./Letras"
 import palavras from "../palavras"
 
 export default function App() {
 
+    const [contador, setContador] = useState(0)
 
     return (
         <div>
             <Jogo palavras={palavras} />
-            <Letras />
+            <Letras contador={contador} setContador={setContador}/>
         </div>
     )
 }
